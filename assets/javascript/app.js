54358c71e790c2616e966a0d1a71dc04c994469e
Becky Afzali <beckyafzali@gmail.com>
2018-03-16 21:49:59 -0500

    set up index.html file

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+const questionCard = {
+  questions: ['Question1', 'Question2', 'Question3'],
+
+};
+
+function questions(question, option1, option2, option3, option4) {
+  this.question = question;
+  this.option1 = option1;
+  this.option2 = option2;
+  this.option3 = option3;
+  this.option4 = option4;
+}
+
